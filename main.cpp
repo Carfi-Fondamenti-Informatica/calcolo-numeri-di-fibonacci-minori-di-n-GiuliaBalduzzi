13bc98980bfdd193b31950e8639bc170bf25d664
GiuliaBalduzzi <91681343+GiuliaBalduzzi@users.noreply.github.com>
2021-11-13 16:24:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int i, n, x=0, y=1, z= x+y;
+    cin >> n;
+    cout << z << endl;
+    for (int i=2; i <= n; i++){
+        z = x + y;
+        x = y;
+        y = z;
+        cout << z << endl;
+    }
+    return 0;
 }
